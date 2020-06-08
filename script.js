138971f800ac53fd7204bc2c90ddba6f01c5270b
Ethan Tatum <ethantatum81@gmail.com>
2020-06-08 11:04:55 -0700

    completed functionality

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharactersArray = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "+", "=", "/"];
+let combinedArray = [];
+
+
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
@@ -11,7 +18,7 @@ writePassword = () => {
 }
 
 generatePassword = () => {
-    let generatedPassword;
+    let generatedPassword = [];
     let passwordLength;
     let lowercase;
     let uppercase;
@@ -23,13 +30,61 @@ generatePassword = () => {
         if(passwordLength < 8 || passwordLength > 128) {
             alert(`Sorry, you need to select between 8 and 128 characters. Please try again.`);
             lengthCheck();
-        } 
+        } else {
+            lowercasePrompt();
+        }
     };
     lengthCheck();
     
-    lowercase = confirm(`Would you like to include lowercase characters? Select "OK" for yes, "Cancel" for no.`);
+    function lowercasePrompt() {
+        lowercase = confirm(`Would you like to include lowercase characters? Select "OK" for yes, "Cancel" for no.`);
+        if(lowercase) {
+            combinedArray.push(lowercaseArray);
+        }
+        uppercasePrompt();
+    };
 
-    return generatedPassword;
+    function uppercasePrompt ()  {
+        uppercase = confirm(`Would you like to include uppercase characters? Select "OK" for yes, "Cancel" for no.`);
+        if(uppercase) {
+            combinedArray.push(uppercaseArray);
+        }
+        numericPrompt();
+    };
+
+    function numericPrompt () {
+        numeric = confirm(`Would you like to include numbers? Select "OK" for yes, "Cancel" for no.`);
+        if(numeric) {
+            combinedArray.push(numericArray);
+        }
+        specialCharsPrompt();
+    };
+
+    function specialCharsPrompt () {
+        specialCharacters = confirm(`Would you like to include special characters? Select "OK" for yes, "Cancel" for no.`);
+        if(specialCharacters) {
+            combinedArray.push(specialCharactersArray);
+        }
+        validator();
+    };
+
+    function validator () {
+        if(combinedArray.length < 1) {
+            alert(`Sorry, you must select at least one type of character to generate a password. Please try again.`);
+            combinedArray = [];
+            lengthCheck();
+        } else {
+            alert(`Let me create that password for you!`);
+            console.log(combinedArray);
+            combinedArray = combinedArray.flat();
+            for(let i = 0; i < passwordLength; i++) {
+                generatedPassword.push(combinedArray[Math.floor(Math.random() * combinedArray.length)]);
+            };
+            combinedArray = [];
+        }
+    };
+
+    return generatedPassword.join("");
 };
 
 // Add event listener to generate button
